65e2ebc0793325c6e641aeaf041d1494cf04cd0c
yt <ytxiaomu@gmail.com>
2015-12-08 00:43:51 +0800

    完成网络模块基础

---
+++ 蓦然/蓦然/YTNetworkManager.h
@@ -0,0 +1,13 @@
+//
+//  YTNetworkManager.h
+//  蓦然
+//
+//  Created by yt on 15/12/3.
+//  Copyright © 2015年 yt. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YTNetworkManager : NSObject
+
+@end
---
+++ 蓦然/蓦然/YTNetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  YTNetworkManager.m
+//  蓦然
+//
+//  Created by yt on 15/12/3.
+//  Copyright © 2015年 yt. All rights reserved.
+//
+
+#import "YTNetworkManager.h"
+
+@implementation YTNetworkManager
+
+@end
